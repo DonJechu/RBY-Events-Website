a3be056eb323bdfb6e8baf17cc066920daa008a4
DonJechu <120075125+DonJechu@users.noreply.github.com>
2023-11-11 23:30:35 -0600

    Add files via upload

---
+++ src/Components/Hero/hero.jsx
@@ -0,0 +1,94 @@
+import React from 'react';
+import { useEffect } from 'react';
+import AOS from 'aos';
+import 'aos/dist/aos.css'; // Import the AOS styles
+
+function HeroSection() {
+    useEffect(() => {
+        AOS.init({
+            // duration: 1000, // Adjust the animation duration as needed
+            // once: true, // Set to true if you want the animation to occur only once
+        });
+    }, []);
+
+
+
+    return (
+        <>
+            <section >
+                <div className="main-hero">
+                    <div>
+                        <h1 className=' hero-heading ' data-aos="fade-up" duration='2000' >
+                            Organize, Create, <h1 data-aos="fade-up" duration='5000' > Inspire </h1>
+                        </h1>
+                        <p className='hero-para' duration='40000' data-aos='fade-up'>Mastering the art of conceptualizing, coordinating, and bringing projects to life, I thrive on creating moments that spark awe. Together, let's craft some digital wonders!</p>
+                    </div>
+                    <div className='btn-div'>
+                        <div>
+                            <button class="c-button c-button--gooey"> Portfolio
+                                <div class="c-button__blobs">
+                                    <div></div>
+                                    <div></div>
+                                    <div></div>
+                                </div>
+                            </button>
+                            <svg xmlns="http://www.w3.org/2000/svg" version="1.1" style={{ display: 'blocl', height: "0", width: '0' }}>
+                                <defs>
+                                    <filter id="goo">
+                                        <feGaussianBlur in="SourceGraphic" stdDeviation="10" result="blur"></feGaussianBlur>
+                                        <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 18 -7" result="goo"></feColorMatrix>
+                                        <feBlend in="SourceGraphic" in2="goo"></feBlend>
+                                    </filter>
+                                </defs>
+                            </svg>
+
+                        </div>
+                        <div>
+                            <button class="c-button c-button--gooey"> Hire a freelancer
+                                <div class="c-button__blobs">
+                                    <div></div>
+                                    <div></div>
+                                    <div></div>
+                                </div>
+                            </button>
+                            <svg xmlns="http://www.w3.org/2000/svg" version="1.1" style={{ display: 'blocl', height: "0", width: '0' }}>
+                                <defs>
+                                    <filter id="goo">
+                                        <feGaussianBlur in="SourceGraphic" stdDeviation="10" result="blur"></feGaussianBlur>
+                                        <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 18 -7" result="goo"></feColorMatrix>
+                                        <feBlend in="SourceGraphic" in2="goo"></feBlend>
+                                    </filter>
+                                </defs>
+                            </svg>
+
+                        </div>
+                    </div>
+
+
+                    <div className="main-brand-div">
+                        <div
+                            data-aos="fade-right" duration='2000'
+                            className='brandd-2'>
+                            <h2 className='brandd-head-2'>FOR CREATORS </h2>
+                            <p className='brand-para'>
+                                Boost your Content Impact
+                            </p>
+                        </div>
+                        <div className='brandd'
+                            data-aos="fade-left" duration='2000' 
+                        >
+                            <h2 className='brandd-head'>FOR BRANDS </h2>
+                            <p className='brand-para'>
+                                Tap into Gaming Audience
+                            </p>
+                        </div>
+
+                    </div>
+                </div>
+            </section>
+        </>
+
+    );
+}
+
+export default HeroSection;
---
+++ src/Components/Services/services.jsx
@@ -0,0 +1,109 @@
+import { TbSwords } from 'react-icons/tb';
+import '../../App.css';
+import { useEffect } from 'react';
+import AOS from 'aos';
+import 'aos/dist/aos.css'; // Import the AOS styles
+
+
+function Service() {
+    useEffect(() => {
+        AOS.init({
+            duration: 1000, // Adjust the animation duration as needed
+            //   once: true, // Set to true if you want the animation to occur only once
+        });
+    }, []);
+    return (
+        <>
+            <section id='service'>
+                <div className='head-service'><h1>Services</h1></div>
+                <div className=" row " >
+
+                    <div className=' service-div' data-aos="fade-up"  >
+                        <div>
+                            <TbSwords className='m-auto   icon' /></div>
+                        <div>
+                            <p>Full Event Development </p>
+                        </div>
+
+                    </div>
+
+                    <div className=' service-div' data-aos="fade-up"  >
+                        <div>
+                            <TbSwords className='m-auto   icon' /></div>
+                        <div>
+                            <p>3D Modeling </p>
+                        </div>
+
+                    </div>
+
+                    <div className=' service-div' data-aos="fade-up" >
+                        <div>
+                            <TbSwords className='m-auto   icon' /></div>
+                        <div>
+                            <p>Plugin Development </p>
+                        </div>
+
+                    </div>
+
+                    <div className=' service-div' data-aos="fade-up"  >
+                        <div>
+                            <TbSwords className='m-auto   icon' /></div>
+                        <div>
+                            <p>Quality Assurance  </p>
+                        </div>
+
+                    </div>
+
+                    <div className=' service-div' data-aos="fade-up" >
+                        <div>
+                            <TbSwords className='m-auto   icon' /></div>
+                        <div>
+                            <p>Creative Direction </p>
+                        </div>
+
+                    </div>
+
+                    <div className=' service-div' data-aos="fade-up"  >
+                        <div>
+                            <TbSwords className='m-auto   icon' /></div>
+                        <div>
+                            <p>Map Creation </p>
+                        </div>
+
+                    </div>
+
+                    <div className=' service-div' data-aos="fade-up" >
+                        <div>
+                            <TbSwords className='m-auto   icon' /></div>
+                        <div>
+                            <p>System Administration </p>
+                        </div>
+
+                    </div>
+
+                    <div className=' service-div' data-aos="fade-up">
+                        <div>
+                            <TbSwords className='m-auto   icon' /></div>
+                        <div>
+                            <p>In-Game cinematics  </p>
+                        </div>
+
+                    </div>
+
+                    <div className=' service-div' data-aos="fade-up" >
+                        <div>
+                            <TbSwords className='m-auto   icon' /></div>
+                        <div>
+                            <p>Server Setups  </p>
+                        </div>
+
+                    </div>
+
+                </div>
+            </section>
+
+        </>
+    )
+}
+
+export default Service;
