b9971d237ec48a3cf5dcfb1df8209a981e657f56
DonJechu <120075125+DonJechu@users.noreply.github.com>
2023-11-19 22:17:15 -0600

    Update work.jsx

--- src/Components/Work/work.jsx
+++ src/Components/Work/work.jsx
@@ -1,6 +1,9 @@
 import './work.css'
 import React, { useEffect } from 'react';
 import work from '../../assests/work.png'
+import work2 from '../../assests/work2.jpg'
+import work3 from '../../assests/work3.png'
+import work4 from '../../assests/work4.png'
 
 import AOS from 'aos';
 import 'aos/dist/aos.css'; // Import the AOS styles
@@ -25,13 +28,13 @@ function Work() {
                             <img src={work} alt="work" />
                         </div>
                         <div>
-                            <img src={work} alt="work" />
+                            <img src={work2} alt="work2" />
                         </div>
                         <div>
-                            <img src={work} alt="work" />
+                            <img src={work3} alt="work3" />
                         </div>
                         <div>
-                            <img src={work} alt="work" />
+                            <img src={work4} alt="work4" />
                         </div>
                     </div>
                 </div>
