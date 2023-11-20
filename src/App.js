9eb6ffb8929b4ac3b6944f3b850050e8834c8fca
DonJechu <120075125+DonJechu@users.noreply.github.com>
2023-11-20 17:37:24 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,22 +7,42 @@ import Service from './Components/Services/services';
 import Statistics from './Components/Statistics/statistics';
 
 import Footer from './Components/Footer/footer.jsx';
-
-import {Analytics} from "@vercel/analytics/react"
+import { Analytics } from '@vercel/analytics/react';
+import { Routes, Route } from 'react-router-dom';
+import ResumeNew from './Components/Resume/ResumeNew.js';
 
 function App() {
 
   return (
     <>
       <div >
+
         <Navbar />
-        <HeroSection />
-        <Statistics />
-        <Service />
-        <Brands />
-        <Footer />
-        <Analytics />
+
+
+        <Routes>
+          <Route path='/' element={<HeroSection />} />
+          <Route path='/resume' element={<ResumeNew />} />
+
+          {/* <Route path='/' element={<Statistics />} />
+
+          <Route path='/' element={<Service />} />
+
+          <Route path='/' element={<Analytics />} />
+
+
+          <Route path='/' element={<Brands />} /> */}
+
+
+
+          {/* </Route> */}
+        </Routes>
+
+
+
+
       </div>
+
     </>
   );
 }
