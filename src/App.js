5efb5b503a7845d0cbd6669216b9755900c1cfcb
DonJechu <120075125+DonJechu@users.noreply.github.com>
2024-03-15 19:33:18 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,8 +9,10 @@ import { Analytics } from '@vercel/analytics/react';
 import { Routes, Route } from 'react-router-dom';
 import ResumeNew from './Components/Portfolio/3D.js';
 import ServerDevelopment from './Components/Portfolio/ServerDevelopment.js';
-import Datapack from './Components/Portfolio/Datapack.js';
 import NotFoundPage from './Components/NotFoundPage/404.js';
+
+import Products from './Components/Products/index.js';
+import Products01 from './Components/Products/p01.js';
 
 function App() {
   return (
@@ -21,7 +23,9 @@ function App() {
           <Route path='/' element={<HeroSection />} />
           <Route path='/portfolio/3d' element={<ResumeNew />} />
           <Route path='/portfolio/server' element={<ServerDevelopment />} />
-          <Route path='/products/datapack' element={<Datapack />} />
+          <Route path='/products' element={<Products />} />
+          <Route path='/products/01' element={<Products01 />} />
+
           {/* Agrega la ruta NotFoundPage */}
           <Route path='*' element={<NotFoundPage />} />
         </Routes>
