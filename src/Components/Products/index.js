a2060f17320a83371da26e4cefff535023492449
DonJechu <120075125+DonJechu@users.noreply.github.com>
2024-03-17 16:42:49 -0600

    Update index.js

--- src/Components/Products/index.js
+++ src/Components/Products/index.js
@@ -1,3 +1,4 @@
+// index.js
 import React, { useState, useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import { Link } from "react-router-dom";
@@ -5,6 +6,8 @@ import { Link } from "react-router-dom";
 import './index.css'; // Importa tu archivo CSS
 import Footer from '../Footer/footer';
 import portfolio1 from '../../assests/products/1.png';
+import portfolio2 from '../../assests/products/2.png';
+import portfolio3 from '../../assests/products/3.png';
 
 function ResumeNew() {
   const [width, setWidth] = useState(window.innerWidth);
@@ -50,12 +53,71 @@ function ResumeNew() {
               </Link>
             </Col>
 
+            {/* Producto 2 */}
+            <Col xs={12} sm={6} md={4} lg={3} className="product-column">
+              <Link to="/products/02" className="product-link">
+                <div className="product-container">
+                  <img
+                    src={portfolio2}
+                    alt="Portfolio2"
+                    className="product-image"
+                  />
+                  <div className="product-name">RBY's Clean Hud</div>
+                  <div className="product-info">
+                    <div className="left-info">Byjechu</div>
+                    <div className="right-info">
+                      {width >= 375 && <div className="new-badge">New🆕</div>}
+                    </div>
+                  </div>
+                </div>
+              </Link>
+            </Col>
+
             {/* Aquí van los demás productos */}
           </Row>
 
           {/* Nueva fila de productos */}
           <Row className="resume justify-content-center align-items-center">
-            {/* Aquí van los productos restantes */}
+            {/* Producto 3 */}
+            <Col xs={12} sm={6} md={4} lg={3} className="product-column">
+              <Link to="/products/03" className="product-link">
+                <div className="product-container">
+                  <img
+                    src={portfolio3}
+                    alt="Portfolio3"
+                    className="product-image"
+                  />
+                  <div className="product-name">RBY Rings</div>
+                  <div className="product-info">
+                    <div className="left-info">Byjechu</div>
+                    <div className="right-info">
+                      {width >= 375 && <div className="new-badge">New🆕</div>}
+                    </div>
+                  </div>
+                </div>
+              </Link>
+            </Col>
+
+            {/* Producto 4 */}
+            <Col xs={12} sm={6} md={4} lg={3} className="product-column">
+              <Link to="/products/01" className="product-link">
+                <div className="product-container">
+                  <img
+                    src={portfolio1}
+                    alt="Portfolio1"
+                    className="product-image"
+                  />
+                  <div className="product-name">Lanterns Enhanced</div>
+                  <div className="product-info">
+                    <div className="left-info">Byjechu</div>
+                    <div className="right-info">
+                      {width >= 375 && <div className="new-badge">New🆕</div>}
+                    </div>
+                  </div>
+                </div>
+              </Link>
+            </Col>
+
           </Row>
         </Container>
       </div>
